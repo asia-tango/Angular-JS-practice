c2cecc6e2a39e00fc194c7bf197bda504ba02936
asia-tango <kaplunoleksandra@gmail.com>
2020-04-02 23:28:53 +0300

    Connection between two controllers

--- controller.js
+++ controller.js
@@ -1,4 +1,23 @@
-angular.module("app", []).controller("myCtrl", function($scope) {
-  $scope.number = 55;
-  $scope.myBook = "AngularJS";
+// angular.module("app", []).controller("myCtrl", function($scope) {
+//   $scope.number = 55;
+//   $scope.myBook = "AngularJS";
+// });
+const app = angular.module("app", []);
+
+app.controller("firstCtrl", function($scope, myFactory) {
+  console.log("first");
+  $scope.myFactory = myFactory;
+  $scope.hello = "hello world";
+});
+
+app.controller("secondCtrl", function($scope, myFactory) {
+  console.log("second");
+  $scope.myFactory = myFactory;
+  $scope.hello = "hello world";
+});
+
+app.factory("myFactory", function() {
+  return {
+    hello: "hello world"
+  };
 });
