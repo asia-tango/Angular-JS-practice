17ec0cb6dd9abd9897bf3089ce0f8aa17f0ca5e6
asia-tango <kaplunoleksandra@gmail.com>
2020-04-03 00:05:19 +0300

    Methods in the controller

--- controller.js
+++ controller.js
@@ -1,23 +1,41 @@
+// ------------------------------------------- Binding and controller
 // angular.module("app", []).controller("myCtrl", function($scope) {
 //   $scope.number = 55;
 //   $scope.myBook = "AngularJS";
 // });
 const app = angular.module("app", []);
+// ------------------------------------------- Connection between 2 controllers
+// app.controller("firstCtrl", function($scope, myFactory) {
+//   console.log("first");
+//   $scope.myFactory = myFactory;
+//   $scope.hello = "hello world";
+// });
 
-app.controller("firstCtrl", function($scope, myFactory) {
-  console.log("first");
-  $scope.myFactory = myFactory;
-  $scope.hello = "hello world";
-});
+// app.controller("secondCtrl", function($scope, myFactory) {
+//   console.log("second");
+//   $scope.myFactory = myFactory;
+//   $scope.hello = "hello world";
+// });
 
-app.controller("secondCtrl", function($scope, myFactory) {
-  console.log("second");
-  $scope.myFactory = myFactory;
+// app.factory("myFactory", function() {
+//   return {
+//     hello: "hello world"
+//   };
+// });
+
+// -------------------------------------------- Methods in the controller
+app.controller("thirdCtrl", function($scope, myFactory) {
   $scope.hello = "hello world";
+  $scope.myFactory = myFactory;
+  $scope.setHello = function(text) {
+    $scope.hello = text;
+  };
 });
 
 app.factory("myFactory", function() {
   return {
-    hello: "hello world"
+    hello: function() {
+      return "hello world";
+    }
   };
 });
